ea238a7921518b1456320a6051d24f306e18da9e
Avadhut Chavan <avadhutch50@gmail.com>
2020-10-23 00:35:11 +0530

    User Entity added

---
+++ src/dao/User.java
@@ -0,0 +1,55 @@
+package dao;
+
+public class User {
+	private int id;
+	private String fname;
+	private String email;
+	private String mobile;
+	private String username;
+	private String password;
+	public User(int id, String fname, String email, String mobile, String username, String password) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.email = email;
+		this.mobile = mobile;
+		this.username = username;
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
